cfd38587d61f5370a704eba38fcfcf12eff03ccf
MacBook <foysal11203004@gmail.com>
2024-03-20 00:30:46 +0600

    add login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ProductController;
+use App\Http\Controllers\Backend\UserController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,16 +16,27 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/',[HomeController::class,'showHomePage']);
 
-Route::get('/order/list',[HomeController::class,'orderList'])->name('order.list');
+Route::group(['prefix' => 'admin'], function () {
 
-Route::get('/product/list',[ProductController::class,'list'])->name('product.list');
+    Route::get('/login', [UserController::class, 'login'])->name('admin.login');
 
-Route::get('/product/create/form',[ProductController::class,'createForm'])->name('product.create.form');
+    
 
-Route::get('/category/list',[CategoryController::class,'list'])->name('category.list');
+    Route::group(['middleware'=>'auth'],function(){
+        Route::get('/', [HomeController::class, 'showHomePage']);
 
-Route::get('/category/form',[CategoryController::class,'categoryForm'])->name('category.form');
-
-Route::post('/category/store',[CategoryController::class,'categoryStore'])->name('category.store');
+        Route::get('/order/list', [HomeController::class, 'orderList'])->name('order.list');
+    
+        Route::get('/product/list', [ProductController::class, 'list'])->name('product.list');
+    
+        Route::get('/product/create/form', [ProductController::class, 'createForm'])->name('product.create.form');
+    
+        Route::get('/category/list', [CategoryController::class, 'list'])->name('category.list');
+    
+        Route::get('/category/form', [CategoryController::class, 'categoryForm'])->name('category.form');
+    
+        Route::post('/category/store', [CategoryController::class, 'categoryStore'])->name('category.store');
+    });
+   
+});
